62b20f03c79f0cd1fbebb168c544f9913482b469
Marcin Romanowski <marcin.romanowski.programista@gmail.com>
2018-09-05 17:58:06 +0200

    updated version 1.1

--- js/balance.js
+++ js/balance.js
@@ -1,33 +1,85 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['Task', 'Amount of money'],
-  ['Food', 8],
-  ['House', 2],
-  ['Transport', 4],
-  ['Telecomunication', 2],
-  ['Healthcare', 8],
-  ['Hygiene', 18],
-  ['Kids', 8],
-  ['Entertainment', 8],
-  ['Trips', 8],
-  ['Trainings', 8],
-  ['Books', 8],
-  ['Savings', 8],
-  ['Pension', 8],
-  ['Repayment', 8],
-  ['Donation', 8],
-  ['Others', 8]
-]);
-
-  var options = {
-	  'title':'Expenses',
-	  'width':600,
-	  'height':400
-	 
-};
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
+Highcharts.chart('piechart', {
+  chart: {
+    plotBackgroundColor: null,
+    plotBorderWidth: null,
+    plotShadow: false,
+    type: 'pie'
+  },
+  title: {
+    text: 'Chart of Expenses'
+  },
+  tooltip: {
+    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+  },
+  plotOptions: {
+    pie: {
+      allowPointSelect: true,
+      cursor: 'pointer',
+      dataLabels: {
+        enabled: true,
+        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+        style: {
+          color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+        }
+      }
+    }
+  },
+  series: [{
+    name: 'Brands',
+    colorByPoint: true,
+    data: [{
+      name: 'Food',
+      y: 5.88,
+      sliced: true,
+      selected: true
+    }, {
+      name: 'House',
+      y: 5.88
+    }, {
+      name: 'Transport',
+      y: 5.88
+    }, {
+      name: 'Telecomunication',
+      y: 5.88
+    }, {
+      name: 'Healthcare',
+      y: 5.88
+    }, {
+      name: 'Cloth',
+      y: 5.88
+    }, {
+      name: 'Hygiene',
+      y: 5.88
+    }, {
+      name: 'Kids',
+      y: 5.88
+    }, {
+      name: 'Entertainment',
+      y: 5.88
+    }, {
+      name: 'Trips',
+      y: 5.88
+    }, {
+      name: 'Trainings',
+      y: 5.88
+    }, {
+      name: 'Books',
+      y: 5.88
+    }, {
+      name: 'Savings',
+      y: 5.88
+    }, {
+      name: 'Pension',
+      y: 5.88
+    }, {
+      name: 'Repayment',
+      y: 5.88
+    }, {
+      name: 'Donation',
+      y: 5.88
+    }, {
+      name: 'Others',
+      y: 5.88
+    }]
+  }]
+});
--- js/clock.js
+++ js/clock.js
@@ -5,6 +5,7 @@ function timer()
 				var today = new Date();
 		
 				var day = today.getDate();
+				if(day < 10) day = "0" + day;
 				var month = today.getMonth() + 1;
 				if(month < 10) month = "0" + month;
 				var year = today.getFullYear();
